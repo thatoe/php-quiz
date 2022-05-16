3a096c1b828f998af2bf6548beb8da0d9753b52b
thatoe <5407906-zoe_tt@users.noreply.gitlab.com>
2022-05-16 14:56:32 +0630

    logic_test

--- logic_test.php
+++ logic_test.php
@@ -1,4 +1,22 @@
 <?php
+// 2. Logic Test 
+// Write a PHP program that pushes half of the zeros in a given array to the beginning of the array, and the other half to the end. 
+// Examples 1: 
+// Input: [3,5,6,0,7,0,1] 
+// Output: [0,3,5,6,7,1,0] 
+// Examples 2: 
+// Input: [5,0,0,6,0,8] 
+// Output: [0,0,5,6,8,0] 
+// Examples 3: 
+// Input: [1,2,3,0,0,0,0] 
+// Output: [0,0,1,2,3,0,0] 
+// Examples 4: 
+// Input: [1,2,3] 
+// Output: [1,2,3] 
+// function transformedArray(array $inputArray) : array { 
+// //your code here 
+// } 
+
 function transformedArray(array $inputArray) : array {
     //your code here
     $zeros = 0;
